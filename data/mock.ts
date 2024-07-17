f1f78dbf541e2bb26bd48f393561c2aa9f02176d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-17 15:08:26 +0900

    Feat: add card mock data

---
+++ data/mock.ts
@@ -0,0 +1,32 @@
+export const CARD_DATA = [
+  {
+    id: 'sha-20wdjnas930m...',
+    size: '124.2MB',
+    tags: 'httpd/latest',
+    status: 'primary',
+  },
+  {
+    id: 'sha-24546wdjnas930m...',
+    size: '35.2MB',
+    tags: 'httpd/latest',
+    status: 'secondary',
+  },
+  {
+    id: 'suyoooi-24546wdjnas930m...',
+    size: '1.2MB',
+    tags: 'httpd/latest',
+    status: 'accent',
+  },
+  {
+    id: 'suyoooi-24546wdjnas930m...',
+    size: '1.2MB',
+    tags: 'httpd/latest',
+    status: 'secondary',
+  },
+  {
+    id: 'suyoooi-24546wdjnas930m...',
+    size: '1.2MB',
+    tags: 'httpd/latest',
+    status: 'primary',
+  },
+];
