f979897fa739b4bb16cd4a8a2b7162003efc8196
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-18 09:32:05 +0900

    Update mock.ts

--- data/mock.ts
+++ data/mock.ts
@@ -1,30 +1,30 @@
 export const CARD_DATA = [
   {
-    id: 'sha-20wdjnas930m...',
+    id: 'sha-image',
     size: '124.2MB',
     tags: 'httpd/latest',
     status: 'primary',
   },
   {
-    id: 'sha-24546wdjnas930m...',
+    id: 'sha-24546wdjnas930m-gkdlxkduwldk',
     size: '35.2MB',
     tags: 'httpd/latest',
     status: 'secondary',
   },
   {
-    id: 'suyoooi-24546wdjnas930m...',
+    id: 'suyoooi-24546wdjnas930m',
     size: '1.2MB',
     tags: 'httpd/latest',
     status: 'accent',
   },
   {
-    id: 'suyoooi-24546wdjnas930m...',
+    id: 'suyoooi-24546wdjnas930m',
     size: '1.2MB',
     tags: 'httpd/latest',
     status: 'secondary',
   },
   {
-    id: 'suyoooi-24546wdjnas930m...',
+    id: 'suyoooi-123123container',
     size: '1.2MB',
     tags: 'httpd/latest',
     status: 'primary',
