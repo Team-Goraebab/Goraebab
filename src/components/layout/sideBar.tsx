23c85cc75237d882c46bb9365d4db1609eea5f5b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-22 18:04:08 +0900

    style: fixed sideBar

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -5,7 +5,7 @@ import { CARD_DATA } from '../../../data/mock';
 
 const Sidebar = () => {
   return (
-    <div className="w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
+    <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
       <div className="flex-grow overflow-y-auto scrollbar-hide">
         {CARD_DATA.map((item, index) => (
           <Card
