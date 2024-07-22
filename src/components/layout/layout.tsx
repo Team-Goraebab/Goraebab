3213226dc392c4a3092078b61b3da7226eac5c82
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-22 18:03:44 +0900

    style: fixed sideBar

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,23 +1,16 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './header';
 import Sidebar from './sideBar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
-
   return (
     <div className="relative flex h-screen bg-basic_1">
+      <Sidebar />
       <div className="flex flex-col flex-1">
         <Header />
-        <Sidebar />
-        {/* <div className="flex-1 overflow-y-auto bg-basic_1 mt-14"> */}
-        <div className="">
+        <div className="flex-1 overflow-y-auto bg-basic_1 mt-14 p-4  ml-[300px]">
           <main>{children}</main>
         </div>
       </div>
