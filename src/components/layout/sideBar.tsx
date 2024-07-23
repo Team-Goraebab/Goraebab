adc5a817cc815f33db2fc503593fc5504686d009
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-24 02:43:41 +0900

    feat: add data props

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Button from '../button/button';
 import Card from '../card/card';
-import { CARD_DATA } from '../../../data/mock';
 
-const Sidebar = () => {
+interface SidebarProps {
+  data: any[];
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ data }) => {
   return (
     <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
       <div className="flex-grow overflow-y-auto scrollbar-hide">
-        {CARD_DATA.map((item, index) => (
+        {data.map((item, index) => (
           <Card
             key={index}
             id={item.id}
