90f9405a4b9b1affb00d1a285a1e828c1d6a4223
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-25 13:41:32 +0900

    feat: create components index for file path

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+// layout
+export { default as Layout } from './layout/layout';
+export { default as Header } from './layout/header';
+export { default as Sidebar } from './layout/sideBar';
+
+// button
+export { default as Button } from './button/button';
+export { default as ZoomButtons } from './button/zoomButtons';
+export { default as PanButtons } from './button/panButtons';
+
+// card
+export { default as Card } from './card/card';
+export { default as HostCard } from './card/hostCard';
+
+// modal
+export { default as Modal } from './modal/modal';
+export { default as OptionModal } from './modal/optionModal';
