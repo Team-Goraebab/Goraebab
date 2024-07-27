2e9ba0b0209b54ae5d09b324e5eb686d32283e71
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-28 03:04:56 +0900

    style: image modal css 수정

--- src/components/modal/imageModal.tsx
+++ src/components/modal/imageModal.tsx
@@ -14,30 +14,31 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
       <div className="relative bg-white p-6 rounded-lg w-11/12 max-w-4xl mx-4 md:mx-0">
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 text-2xl"
+          className="absolute top-4 right-4 text-grey_4 hover:text-grey_4 text-2xl"
         >
           &times;
         </button>
         <h2 className="text-lg font-semibold mb-4 text-center">
-          <span className="text-blue-500">
-            이미지를 불러올 방식을 선택하세요.
+          <span className="text-blue_2">
+            이미지
+            <span className="text-black">를 불러올 방식을 선택하세요.</span>
           </span>
         </h2>
-        <p className="text-center text-gray-600 mb-6">
+        <p className="text-center text-black mb-6">
           Please choose a method to upload the image.
         </p>
-        <div className="flex justify-around">
-          <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6 text-center cursor-pointer hover:border-gray-500 w-1/3">
-            <FaFolderOpen className="text-gray-600 mb-4" size={50} />
-            <p className="font-semibold">Local Path</p>
-            <p className="text-sm text-gray-500 mt-2">
+        <div className="flex justify-around gap-6">
+          <div className="flex flex-col items-center border border-grey_3 rounded-lg p-6 text-center cursor-pointer hover:border-grey_4 w-1/2">
+            <FaFolderOpen className="text-grey_4 mb-4" size={50} />
+            <p className="font-semibold text-grey_4">Local Path</p>
+            <p className="text-sm text-grey_4 mt-2">
               내 컴퓨터에서 도커 이미지를 가져옵니다.
             </p>
           </div>
-          <div className="flex flex-col items-center border border-gray-300 rounded-lg p-6 text-center cursor-pointer hover:border-gray-500 w-1/3">
-            <FaDocker className="text-gray-600 mb-4" size={50} />
-            <p className="font-semibold">Docker Hub</p>
-            <p className="text-sm text-gray-500 mt-2">
+          <div className="flex flex-col items-center border border-grey_3 rounded-lg p-6 text-center cursor-pointer hover:border-grey_4 w-1/2">
+            <FaDocker className="text-grey_4 mb-4" size={50} />
+            <p className="font-semibold text-grey_4">Docker Hub</p>
+            <p className="text-sm text-grey_4 mt-2">
               도커 허브에서 이미지를 가져옵니다.
             </p>
           </div>
