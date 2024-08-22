0accbd70aaa33dd2853629239e29c726f7cfbc35
yeoniii20 <commetoi_yeoni@naver.com>
2024-08-22 10:28:34 +0900

    Refactor: 사용하지 않는 파일 삭제

--- src/app/(volume)/volume/page.tsx
+++ src/app/(volume)/volume/page.tsx
@@ -1,18 +1,9 @@
 'use client';
 
-import ContainersManagement from '@/components/docker/containersManagement';
-import DBManagement from '@/components/docker/dbManagement';
-import DockerManagement from '@/components/docker/dockerManagement';
-import HarborManagement from '@/components/docker/harborManagement';
-
 const VolumePage = () => {
   return (
     <>
       <h1>Management Dashboard</h1>
-      <DockerManagement />
-      <DBManagement />
-      <HarborManagement />
-      <ContainersManagement />
     </>
   );
 };
