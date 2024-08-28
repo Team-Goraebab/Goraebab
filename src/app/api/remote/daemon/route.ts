7d22ad5c4586b6c0c1c23d3c6db997e4bc7fce9c
yeoniii20 <commetoi_yeoni@naver.com>
2024-08-28 19:11:43 +0900

    Feat: add remote daemon api

--- src/app/api/remote/daemon/route.ts
+++ src/app/api/remote/daemon/route.ts
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 export async function GET(req: NextRequest) {
   try {
-    const response = await axios.get('http://localhost:2375/docker');
+    const response = await axios.get('http://localhost:2387/remote/daemons');
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
-    console.error('Error fetching Docker list:', error);
+    console.error('Error connecting to Daemon:', error);
     return NextResponse.json(
-      { error: 'Failed to fetch Docker list' },
+      { error: 'Failed to connect to Daemon' },
       { status: 500 }
     );
   }
