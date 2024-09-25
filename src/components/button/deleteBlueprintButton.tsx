50d478ed84e14019291fc7f64c078bfa24554609
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-09-25 15:49:52 +0900

    Feat: Create deleteBlueprintButton

---
+++ src/components/button/deleteBlueprintButton.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useHostStore } from '@/store/hostStore';
+
+const DeleteBlueprintButton = () => {
+  const deleteAllHosts = useHostStore((state) => state.deleteAllHosts);
+
+  const handleDelete = () => {
+    if (confirm('설계도를 삭제하시겠습니까?')) {
+      deleteAllHosts();
+    }
+  };
+
+  return (
+    <div className="fixed bottom-4 right-[140px] transform translate-x-4 h-[50px] p-3 text-red_6 hover:text-white bg-white hover:bg-red_4 active:bg-red_6 rounded-lg shadow-lg flex items-center justify-between">
+      <button className="px-4 py-2 text-center" onClick={handleDelete}>
+        Delete All
+      </button>
+    </div>
+  );
+};
+
+export default DeleteBlueprintButton;
