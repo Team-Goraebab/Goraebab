c9a4cdcccfed7bd12e78a0841aab94bbd9ce2e3f
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-09-25 17:40:43 +0900

    Feat: add daemon connect bar

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -48,73 +48,78 @@ const Sidebar = ({ progress }: SidebarProps) => {
   // 네트워크 아이디 === 3
   // 볼륨 아이디 === 4
   return (
-    <div className="fixed top-0 left-0 w-[300px] h-full flex flex-col bg-white p-4 border-r-2 border-grey_2 pt-20">
-      <div className="flex-grow overflow-y-auto scrollbar-hide">
-        {activeId === 1 ? (
-          <>
-            {containerData.length > 0 ? (
-              containerData.map((container, index) => (
-                <ContainerCard key={index} data={container} />
-              ))
-            ) : (
-              <p className={'font-pretendard font-light'}>
-                컨테이너를 추가하세요
-              </p>
-            )}
-          </>
-        ) : activeId === 2 ? (
-          <>
-            {images.length > 0 ? (
-              images.map((image) => <ImageCard key={image.id} data={image} />)
-            ) : (
-              <p className={'font-pretendard font-light'}>
-                이미지를 추가하세요
-              </p>
-            )}
-          </>
-        ) : activeId === 3 ? (
-          <>
-            {networkData.length > 0 ? (
-              networkData.map((network, index) => (
-                <NetworkCard key={index} data={network} />
-              ))
-            ) : (
-              <p className={'font-pretendard font-light'}>
-                네트워크 데이터를 추가하세요
-              </p>
-            )}
-          </>
-        ) : activeId === 4 ? (
-          <>
-            {volumeData.length > 0 ? (
-              volumeData.map((volume, index) => (
-                <VolumeCard key={index} data={volume} />
-              ))
-            ) : (
-              <p className={'font-pretendard font-light'}>
-                볼륨 데이터를 추가하세요
-              </p>
-            )}
-          </>
-        ) : (
-          <></>
-        )}
+    <div className="fixed top-0 left-0 w-[300px] flex flex-col h-full bg-white border-r-2 border-grey_2">
+      {/* This div will take the remaining height above the DaemonConnectBar */}
+      <div className="flex flex-col flex-grow pl-4 pr-4 pt-20 overflow-y-auto scrollbar-hide">
+        <div className="flex-grow">
+          {activeId === 1 ? (
+            <>
+              {containerData.length > 0 ? (
+                containerData.map((container, index) => (
+                  <ContainerCard key={index} data={container} />
+                ))
+              ) : (
+                <p className={'font-pretendard font-light'}>
+                  컨테이너를 추가하세요
+                </p>
+              )}
+            </>
+          ) : activeId === 2 ? (
+            <>
+              {images.length > 0 ? (
+                images.map((image) => <ImageCard key={image.id} data={image} />)
+              ) : (
+                <p className={'font-pretendard font-light'}>
+                  이미지를 추가하세요
+                </p>
+              )}
+            </>
+          ) : activeId === 3 ? (
+            <>
+              {networkData.length > 0 ? (
+                networkData.map((network, index) => (
+                  <NetworkCard key={index} data={network} />
+                ))
+              ) : (
+                <p className={'font-pretendard font-light'}>
+                  네트워크 데이터를 추가하세요
+                </p>
+              )}
+            </>
+          ) : activeId === 4 ? (
+            <>
+              {volumeData.length > 0 ? (
+                volumeData.map((volume, index) => (
+                  <VolumeCard key={index} data={volume} />
+                ))
+              ) : (
+                <p className={'font-pretendard font-light'}>
+                  볼륨 데이터를 추가하세요
+                </p>
+              )}
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="flex-shrink-0">
+          {/* <ProgressBar progress={progress} /> */}
+          {activeId === 1 ? (
+            <AddContainerButton onCreate={handleCreateContainer} />
+          ) : activeId === 2 ? (
+            <AddImageButton onCreate={handleCreateImage} />
+          ) : activeId === 3 ? (
+            <AddBridgeButton onCreate={handleCreateNetwork} />
+          ) : activeId === 4 ? (
+            <AddVolumeButton onCreate={handleCreateVolume} />
+          ) : (
+            <Button title={'추가하기'} />
+          )}
+        </div>
       </div>
-      <div className="flex-shrink-0">
-        {/* <ProgressBar progress={progress} /> */}
-        {activeId === 1 ? (
-          <AddContainerButton onCreate={handleCreateContainer} />
-        ) : activeId === 2 ? (
-          <AddImageButton onCreate={handleCreateImage} />
-        ) : activeId === 3 ? (
-          <AddBridgeButton onCreate={handleCreateNetwork} />
-        ) : activeId === 4 ? (
-          <AddVolumeButton onCreate={handleCreateVolume} />
-        ) : (
-          <Button title={'추가하기'} />
-        )}
+      <div className="">
+        <DaemonConnectBar />
       </div>
-      <DaemonConnectBar />
     </div>
   );
 };
