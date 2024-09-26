3210acbae93c40ebc785e08cd9a133ee8f6d41e3
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-09-26 09:51:46 +0900

    Refactor: change style and refactor code

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Button, ProgressBar } from '@/components';
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import { Button } from '@/components';
 import AddBridgeButton from '../button/addBridgeButton';
 import NetworkCard from '../card/networkCard';
 import VolumeCard from '../card/volumeCard';
@@ -13,10 +13,16 @@ import ImageCard from '../card/imageCard';
 import ContainerCard from '../card/containerCard';
 import { useImageStore } from '@/store/imageStore';
 import DaemonConnectBar from '../bar/daemonConnectBar';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 interface SidebarProps {
   progress: number;
 }
+
+type DataHandlerType = {
+  data: any[];
+  setData: Dispatch<SetStateAction<any[]>>;
+};
 
 const Sidebar = ({ progress }: SidebarProps) => {
   const { activeId } = useMenuStore();
@@ -27,91 +33,75 @@ const Sidebar = ({ progress }: SidebarProps) => {
   const [containerData, setContainerData] = useState<any[]>([]);
   const [imageData, setImageData] = useState<any[]>([]);
 
-  const handleCreateNetwork = (newNetwork: any) => {
-    setNetworkData((prevNetworks) => [...prevNetworks, newNetwork]);
+  const dataHandlers: Record<1 | 2 | 3 | 4, DataHandlerType> = {
+    1: { data: containerData, setData: setContainerData },
+    2: { data: imageData, setData: setImageData },
+    3: { data: networkData, setData: setNetworkData },
+    4: { data: volumeData, setData: setVolumeData },
   };
 
-  const handleCreateVolume = (newVolume: any) => {
-    setVolumeData((prevVolumes) => [...prevVolumes, newVolume]);
+  const handleCreate = (newItem: any) => {
+    if (dataHandlers[activeId as 1 | 2 | 3 | 4]) {
+      const { setData } = dataHandlers[activeId as 1 | 2 | 3 | 4];
+      setData((prevData) => [...prevData, newItem]);
+    }
   };
 
-  const handleCreateContainer = (newContainer: any) => {
-    setContainerData((prevContainers) => [...prevContainers, newContainer]);
+  const componentMap = {
+    1: {
+      addButton: AddContainerButton,
+      cardComponent: ContainerCard,
+      noDataMessage: '컨테이너를 추가하세요',
+    },
+    2: {
+      addButton: AddImageButton,
+      cardComponent: ImageCard,
+      noDataMessage: '이미지를 추가하세요',
+    },
+    3: {
+      addButton: AddBridgeButton,
+      cardComponent: NetworkCard,
+      noDataMessage: '네트워크 데이터를 추가하세요',
+    },
+    4: {
+      addButton: AddVolumeButton,
+      cardComponent: VolumeCard,
+      noDataMessage: '볼륨 데이터를 추가하세요',
+    },
   };
 
-  const handleCreateImage = (newImage: any) => {
-    setImageData((prevImages) => [...prevImages, newImage]);
+  const currentComponent = componentMap[activeId as 1 | 2 | 3 | 4];
+
+  const renderNoDataMessage = (message: string) => (
+    <div className="flex flex-col items-center justify-center text-center p-4 border border-dashed border-blue_3 rounded-md bg-blue_0">
+      <AiOutlineInfoCircle className="text-blue_6 text-2xl mb-2" />
+      {/* Add an icon */}
+      <p className="font-pretendard font-medium text-blue_6">{message}</p>
+    </div>
+  );
+
+  const renderDataList = () => {
+    if (!currentComponent) return null;
+
+    const { cardComponent: CardComponent, noDataMessage } = currentComponent;
+    const data =
+      activeId === 2 ? images : dataHandlers[activeId as 1 | 2 | 3 | 4]?.data;
+
+    return data && data.length > 0
+      ? data.map((item, index) => <CardComponent key={index} data={item} />)
+      : renderNoDataMessage(noDataMessage);
   };
 
-  // 컨테이너 아이디 === 1
-  // 이미지 아이디 === 2
-  // 네트워크 아이디 === 3
-  // 볼륨 아이디 === 4
   return (
     <div className="fixed top-0 left-0 w-[300px] flex flex-col h-full bg-white border-r-2 border-grey_2">
-      {/* This div will take the remaining height above the DaemonConnectBar */}
       <div className="flex flex-col flex-grow pl-4 pr-4 pt-20 overflow-y-auto scrollbar-hide">
-        <div className="flex-grow">
-          {activeId === 1 ? (
-            <>
-              {containerData.length > 0 ? (
-                containerData.map((container, index) => (
-                  <ContainerCard key={index} data={container} />
-                ))
-              ) : (
-                <p className={'font-pretendard font-light'}>
-                  컨테이너를 추가하세요
-                </p>
-              )}
-            </>
-          ) : activeId === 2 ? (
-            <>
-              {images.length > 0 ? (
-                images.map((image) => <ImageCard key={image.id} data={image} />)
-              ) : (
-                <p className={'font-pretendard font-light'}>
-                  이미지를 추가하세요
-                </p>
-              )}
-            </>
-          ) : activeId === 3 ? (
-            <>
-              {networkData.length > 0 ? (
-                networkData.map((network, index) => (
-                  <NetworkCard key={index} data={network} />
-                ))
-              ) : (
-                <p className={'font-pretendard font-light'}>
-                  네트워크 데이터를 추가하세요
-                </p>
-              )}
-            </>
-          ) : activeId === 4 ? (
-            <>
-              {volumeData.length > 0 ? (
-                volumeData.map((volume, index) => (
-                  <VolumeCard key={index} data={volume} />
-                ))
-              ) : (
-                <p className={'font-pretendard font-light'}>
-                  볼륨 데이터를 추가하세요
-                </p>
-              )}
-            </>
-          ) : (
-            <></>
-          )}
-        </div>
+        <div className="flex-grow">{renderDataList()}</div>
         <div className="flex-shrink-0">
           {/* <ProgressBar progress={progress} /> */}
-          {activeId === 1 ? (
-            <AddContainerButton onCreate={handleCreateContainer} />
-          ) : activeId === 2 ? (
-            <AddImageButton onCreate={handleCreateImage} />
-          ) : activeId === 3 ? (
-            <AddBridgeButton onCreate={handleCreateNetwork} />
-          ) : activeId === 4 ? (
-            <AddVolumeButton onCreate={handleCreateVolume} />
+          {currentComponent ? (
+            React.createElement(currentComponent.addButton, {
+              onCreate: handleCreate,
+            })
           ) : (
             <Button title={'추가하기'} />
           )}
