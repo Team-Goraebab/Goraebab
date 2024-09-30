ff301a805612b10071c7fa47a42cf16806c3fd9d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-09-30 17:07:58 +0900

    Feat: create remote harbor service api

---
+++ src/services/harbor/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { BASE_URL, REMOTE_HARBORS } from '@/app/api/urlPath';
+
+export const getRemoteHarbors = async () => {
+  const response = await axios.get(`${BASE_URL}${REMOTE_HARBORS}`, {
+    params: {},
+  });
+  return response.data;
+};
