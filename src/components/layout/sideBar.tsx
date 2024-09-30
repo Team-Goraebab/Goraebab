a127484711cb50aa644a0e015fb3fbf5e5c23263
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-09-30 17:08:41 +0900

    Fix: component error

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -14,6 +14,7 @@ import ContainerCard from '../card/containerCard';
 import { useImageStore } from '@/store/imageStore';
 import DaemonConnectBar from '../bar/daemonConnectBar';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
+import LargeButton from '../button/largeButton';
 
 interface SidebarProps {
   progress: number;
@@ -103,7 +104,7 @@ const Sidebar = ({ progress }: SidebarProps) => {
               onCreate: handleCreate,
             })
           ) : (
-            <Button title={'추가하기'} />
+            <LargeButton title={'추가하기'} onClick={() => {}} />
           )}
         </div>
       </div>
