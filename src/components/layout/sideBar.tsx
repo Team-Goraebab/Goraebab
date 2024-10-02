445cc8da493bdb692194eaa0d1a4c61d68dc163d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-02 16:47:33 +0900

    Update: add api call

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+// Sidebar.tsx
 import React, { useState, Dispatch, SetStateAction, useEffect } from 'react';
 import AddBridgeButton from '../button/addBridgeButton';
 import NetworkCard from '../card/networkCard';
@@ -10,119 +9,99 @@ import AddImageButton from '../button/addImageButton';
 import { useMenuStore } from '@/store/menuStore';
 import ImageCard from '../card/imageCard';
 import ContainerCard from '../card/containerCard';
-import { useImageStore } from '@/store/imageStore';
 import DaemonConnectBar from '../bar/daemonConnectBar';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import LargeButton from '../button/largeButton';
-import axios from 'axios';
+import { fetchData } from '@/services/apiUtils'; // 분리된 API 유틸 함수 가져오기
 
 interface SidebarProps {
   progress: number;
 }
 
-type DataHandlerType = {
-  data: any[];
-  setData: Dispatch<SetStateAction<any[]>>;
-};
-
 const Sidebar = ({ progress }: SidebarProps) => {
   const { activeId } = useMenuStore();
-  const images = useImageStore((state) => state.images);
 
   const [networkData, setNetworkData] = useState<any[]>([]);
   const [volumeData, setVolumeData] = useState<any[]>([]);
   const [containerData, setContainerData] = useState<any[]>([]);
   const [imageData, setImageData] = useState<any[]>([]);
 
-  const dataHandlers: Record<1 | 2 | 3 | 4, DataHandlerType> = {
-    1: { data: containerData, setData: setContainerData },
-    2: { data: imageData, setData: setImageData },
-    3: { data: networkData, setData: setNetworkData },
-    4: { data: volumeData, setData: setVolumeData },
+  // 데이터 로드 함수들
+  const loadVolumeData = async () => {
+    const data = await fetchData('/api/volume/list');
+    setVolumeData(data?.Volumes || []);
   };
 
-  const handleCreate = (newItem: any) => {
-    if (dataHandlers[activeId as 1 | 2 | 3 | 4]) {
-      const { setData } = dataHandlers[activeId as 1 | 2 | 3 | 4];
-      setData((prevData) => [...prevData, newItem]);
-    }
+  const loadNetworkData = async () => {
+    const data = await fetchData('/api/network/list');
+    setNetworkData(data?.Networks || []);
   };
 
-  const componentMap = {
-    1: {
-      addButton: AddContainerButton,
-      cardComponent: ContainerCard,
-      noDataMessage: '컨테이너를 추가하세요',
-    },
-    2: {
-      addButton: AddImageButton,
-      cardComponent: ImageCard,
-      noDataMessage: '이미지를 추가하세요',
-    },
-    3: {
-      addButton: AddBridgeButton,
-      cardComponent: NetworkCard,
-      noDataMessage: '네트워크 데이터를 추가하세요',
-    },
-    4: {
-      addButton: AddVolumeButton,
-      cardComponent: VolumeCard,
-      noDataMessage: '볼륨 데이터를 추가하세요',
-    },
+  const loadContainerData = async () => {
+    const data = await fetchData('/api/container/list');
+    setContainerData(data?.Containers || []);
   };
 
-  const currentComponent = componentMap[activeId as 1 | 2 | 3 | 4];
+  const loadImageData = async () => {
+    const data = await fetchData('/api/image/list');
+    setImageData(data?.Images || []);
+  };
 
   const renderNoDataMessage = (message: string) => (
     <div className="flex flex-col items-center justify-center text-center p-4 border border-dashed border-blue_3 rounded-md bg-blue_0">
       <AiOutlineInfoCircle className="text-blue_6 text-2xl mb-2" />
-      {/* Add an icon */}
       <p className="font-pretendard font-medium text-blue_6">{message}</p>
     </div>
   );
 
   const renderDataList = () => {
-    if (!currentComponent) return null;
+    let data: any[] = [];
+    let CardComponent: React.ElementType | null = null;
+    let noDataMessage = '';
 
-    const { cardComponent: CardComponent, noDataMessage } = currentComponent;
-    const data =
-      activeId === 2 ? images : dataHandlers[activeId as 1 | 2 | 3 | 4]?.data;
+    if (activeId === 1) {
+      data = containerData;
+      CardComponent = ContainerCard;
+      noDataMessage = '컨테이너를 추가하세요';
+    } else if (activeId === 2) {
+      data = imageData;
+      CardComponent = ImageCard;
+      noDataMessage = '이미지를 추가하세요';
+    } else if (activeId === 3) {
+      data = networkData;
+      CardComponent = NetworkCard;
+      noDataMessage = '네트워크 데이터를 추가하세요';
+    } else if (activeId === 4) {
+      data = volumeData;
+      CardComponent = VolumeCard;
+      noDataMessage = '볼륨 데이터를 추가하세요';
+    }
 
-    return data && data.length > 0
+    // CardComponent가 null이 아닐 때만 JSX로 렌더링
+    return data && data.length > 0 && CardComponent
       ? data.map((item, index) => <CardComponent key={index} data={item} />)
       : renderNoDataMessage(noDataMessage);
   };
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('/api/volume/list', {
-        params: {},
-      });
-      console.log('response >>', response.data);
-      return response;
-    } catch (error) {
-      console.error('Error fetching volumes:', error);
-      return [];
-    }
-  };
-
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (activeId === 1) {
+      loadContainerData();
+    } else if (activeId === 2) {
+      loadImageData();
+    } else if (activeId === 3) {
+      loadNetworkData();
+    } else if (activeId === 4) {
+      loadVolumeData();
+    }
+  }, [activeId]); // activeId가 변경될 때마다 해당 데이터를 가져옴
 
   return (
     <div className="fixed top-0 left-0 w-[300px] flex flex-col h-full bg-white border-r-2 border-grey_2">
       <div className="flex flex-col flex-grow pl-4 pr-4 pt-20 overflow-y-auto scrollbar-hide">
         <div className="flex-grow">{renderDataList()}</div>
         <div className="flex-shrink-0">
-          {/* <ProgressBar progress={progress} /> */}
-          {currentComponent ? (
-            React.createElement(currentComponent.addButton, {
-              onCreate: handleCreate,
-            })
-          ) : (
-            <LargeButton title={'추가하기'} onClick={() => {}} />
-          )}
+          {/* 현재 활성화된 데이터에 따라 추가 버튼 표시 */}
+          <LargeButton title={'추가하기'} onClick={() => {}} />
         </div>
       </div>
       <div className="">
