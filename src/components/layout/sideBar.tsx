25267c3395a460d5cc02c21a5036255c5b55f411
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-04 17:20:46 +0900

    Fix: button bug

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -1,5 +1,7 @@
-// Sidebar.tsx
-import React, { useState, Dispatch, SetStateAction, useEffect } from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Button } from '@/components';
 import AddBridgeButton from '../button/addBridgeButton';
 import NetworkCard from '../card/networkCard';
 import VolumeCard from '../card/volumeCard';
@@ -12,21 +14,25 @@ import ContainerCard from '../card/containerCard';
 import DaemonConnectBar from '../bar/daemonConnectBar';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import LargeButton from '../button/largeButton';
-import { fetchData } from '@/services/apiUtils'; // 분리된 API 유틸 함수 가져오기
+import { fetchData } from '@/services/apiUtils';
 
 interface SidebarProps {
   progress: number;
 }
 
+type DataHandlerType = {
+  data: any[];
+  setData: React.Dispatch<React.SetStateAction<any[]>>;
+};
+
 const Sidebar = ({ progress }: SidebarProps) => {
   const { activeId } = useMenuStore();
-
   const [networkData, setNetworkData] = useState<any[]>([]);
   const [volumeData, setVolumeData] = useState<any[]>([]);
   const [containerData, setContainerData] = useState<any[]>([]);
   const [imageData, setImageData] = useState<any[]>([]);
 
-  // 데이터 로드 함수들
+  // API에서 데이터를 로드하는 함수들
   const loadVolumeData = async () => {
     const data = await fetchData('/api/volume/list');
     setVolumeData(data?.Volumes || []);
@@ -39,7 +45,7 @@ const Sidebar = ({ progress }: SidebarProps) => {
 
   const loadContainerData = async () => {
     const data = await fetchData('/api/container/list');
-    setContainerData(data?.Containers || []);
+    setContainerData(data || []);
   };
 
   const loadImageData = async () => {
@@ -47,6 +53,47 @@ const Sidebar = ({ progress }: SidebarProps) => {
     setImageData(data?.Images || []);
   };
 
+  // 데이터를 관리하는 핸들러 매핑
+  const dataHandlers: Record<1 | 2 | 3 | 4, DataHandlerType> = {
+    1: { data: containerData, setData: setContainerData },
+    2: { data: imageData, setData: setImageData },
+    3: { data: networkData, setData: setNetworkData },
+    4: { data: volumeData, setData: setVolumeData },
+  };
+
+  const handleCreate = (newItem: any) => {
+    if (dataHandlers[activeId as 1 | 2 | 3 | 4]) {
+      const { setData } = dataHandlers[activeId as 1 | 2 | 3 | 4];
+      setData((prevData) => [...prevData, newItem]);
+    }
+  };
+
+  const componentMap = {
+    1: {
+      addButton: AddContainerButton,
+      cardComponent: ContainerCard,
+      noDataMessage: '컨테이너를 추가하세요',
+    },
+    2: {
+      addButton: AddImageButton,
+      cardComponent: ImageCard,
+      noDataMessage: '이미지를 추가하세요',
+    },
+    3: {
+      addButton: AddBridgeButton,
+      cardComponent: NetworkCard,
+      noDataMessage: '네트워크 데이터를 추가하세요',
+    },
+    4: {
+      addButton: AddVolumeButton,
+      cardComponent: VolumeCard,
+      noDataMessage: '볼륨 데이터를 추가하세요',
+    },
+  };
+
+  const currentComponent = componentMap[activeId as 1 | 2 | 3 | 4];
+
+  // No data 메시지 표시 함수
   const renderNoDataMessage = (message: string) => (
     <div className="flex flex-col items-center justify-center text-center p-4 border border-dashed border-blue_3 rounded-md bg-blue_0">
       <AiOutlineInfoCircle className="text-blue_6 text-2xl mb-2" />
@@ -54,35 +101,19 @@ const Sidebar = ({ progress }: SidebarProps) => {
     </div>
   );
 
+  // 데이터를 렌더링하는 함수
   const renderDataList = () => {
-    let data: any[] = [];
-    let CardComponent: React.ElementType | null = null;
-    let noDataMessage = '';
+    if (!currentComponent) return null;
 
-    if (activeId === 1) {
-      data = containerData;
-      CardComponent = ContainerCard;
-      noDataMessage = '컨테이너를 추가하세요';
-    } else if (activeId === 2) {
-      data = imageData;
-      CardComponent = ImageCard;
-      noDataMessage = '이미지를 추가하세요';
-    } else if (activeId === 3) {
-      data = networkData;
-      CardComponent = NetworkCard;
-      noDataMessage = '네트워크 데이터를 추가하세요';
-    } else if (activeId === 4) {
-      data = volumeData;
-      CardComponent = VolumeCard;
-      noDataMessage = '볼륨 데이터를 추가하세요';
-    }
+    const { cardComponent: CardComponent, noDataMessage } = currentComponent;
+    const data = dataHandlers[activeId as 1 | 2 | 3 | 4]?.data;
 
-    // CardComponent가 null이 아닐 때만 JSX로 렌더링
-    return data && data.length > 0 && CardComponent
+    return data && data.length > 0
       ? data.map((item, index) => <CardComponent key={index} data={item} />)
       : renderNoDataMessage(noDataMessage);
   };
 
+  // activeId 변경 시 데이터 로드
   useEffect(() => {
     if (activeId === 1) {
       loadContainerData();
@@ -93,15 +124,20 @@ const Sidebar = ({ progress }: SidebarProps) => {
     } else if (activeId === 4) {
       loadVolumeData();
     }
-  }, [activeId]); // activeId가 변경될 때마다 해당 데이터를 가져옴
+  }, [activeId])
 
   return (
     <div className="fixed top-0 left-0 w-[300px] flex flex-col h-full bg-white border-r-2 border-grey_2">
       <div className="flex flex-col flex-grow pl-4 pr-4 pt-20 overflow-y-auto scrollbar-hide">
         <div className="flex-grow">{renderDataList()}</div>
         <div className="flex-shrink-0">
-          {/* 현재 활성화된 데이터에 따라 추가 버튼 표시 */}
-          <LargeButton title={'추가하기'} onClick={() => {}} />
+          {currentComponent ? (
+            React.createElement(currentComponent.addButton, {
+              onCreate: handleCreate,
+            })
+          ) : (
+            <LargeButton title={'추가하기'} onClick={() => {}} />
+          )}
         </div>
       </div>
       <div className="">
