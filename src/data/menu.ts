587db8f8434040ea785a17b5ffc6081ba5d962c2
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-10 17:41:34 +0900

    Fix: edit menu

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -12,15 +12,17 @@ export const MENU_ITEMS = [
   { id: 4, name: 'Volume', path: '/', icon: FaDatabase },
 ];
 
-export const SETTINGS_MENU = [
-  { id: 1, label: 'General', icon: FiSettings },
-  { id: 2, label: 'Resources', icon: FiCpu },
-  { id: 3, label: 'Docker Engine', icon: FiGrid },
-  { id: 4, label: 'Builders', icon: FiTool },
-  { id: 5, label: 'Kubernetes', icon: FiTool },
-  { id: 6, label: 'Software updates', icon: FiBell },
-  { id: 7, label: 'Extensions', icon: FiTool },
-  { id: 8, label: 'Features in development', icon: FiTool },
-  { id: 9, label: 'Notifications', icon: FiBell },
-  { id: 10, label: 'Advanced', icon: FiTool },
+export const MANAGEMENT_MENU = [
+  // 일반 적인 정보 (도커 데몬의 기본 정보 혹은 버전 정보)
+  { id: 1, label: 'General', icon: FiSettings, component: 'General' },
+  // 시스템 자원 사용량 (메모리, CPU, 디스크) 관리 및 모니터링
+  { id: 2, label: 'Resources', icon: FiCpu, component: 'Resources' },
+  // 도커 엔진의 상태와 관련된 설정
+  { id: 3, label: 'Docker Engine', icon: FiGrid, component: 'Engine' },
+  // 빌더 관련 정보로, 도커 이미지 빌드 시 관련된 설정과 빌더들
+  // { id: 4, label: 'Builders', icon: FiTool, component: 'Builders' },
+  // 쿠버네티스 api를 사용해야 해서 일단 보류..
+  // { id: 5, label: 'Kubernetes', icon: FiTool },
+  // 시스템 알림을 관리하며, 도커 이벤트를 트리거할 수 있는 요소들에 대한 알림을 설정
+  // { id: 6, label: 'Notifications', icon: FiBell, component: 'Notifications' },
 ];
