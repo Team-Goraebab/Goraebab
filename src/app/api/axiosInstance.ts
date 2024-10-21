2ef2e16140131a565ad4e4748a580ebfa4a8a1ca
yeoniii20 <commetoi_yeoni@naver.com>
2024-10-21 22:19:18 +0900

    Update: axiosInstance

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -3,11 +3,22 @@ import axios from 'axios';
 export const createDockerClient = (hostIp?: string) => {
   const isWindows = process.platform === 'win32';
   const effectiveHost = hostIp || 'localhost';
-  const baseURL = isWindows
-    ? `http://${effectiveHost}:2375`
-    : `http://${effectiveHost}`;
 
-  return axios.create({
-    baseURL,
-  });
+  const options = isWindows
+    ? { baseURL: `http://${effectiveHost}:2375` }
+    : {
+        baseURL: `http://${effectiveHost}`,
+        socketPath: '/var/run/docker.sock',
+      };
+
+  return axios.create(options);
 };
+
+// export const createDockerClient = () => {
+//   const isWindows = process.platform === 'win32';
+//   const options = isWindows
+//     ? { baseURL: `${process.env.DOCKER_URL || 'http://localhost:2375'}` }
+//     : { baseURL: 'http://localhost', socketPath: '/var/run/docker.sock' };
+
+//   return axios.create(options);
+// };
