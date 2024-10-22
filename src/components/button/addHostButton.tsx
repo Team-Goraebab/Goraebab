cc96d009877be5fc436a32c5d753ced0d6d8bbd8
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-22 14:59:46 +0900

    Fix: Change theme color and modify handler

--- src/components/button/addHostButton.tsx
+++ src/components/button/addHostButton.tsx
@@ -1,96 +1,11 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import HostModal from '../modal/host/hostModal';
-import { v4 as uuidv4 } from 'uuid';
-import { useHostStore } from '@/store/hostStore';
-import { useSnackbar } from 'notistack';
-import { showSnackbar } from '@/utils/toastUtils';
-import { selectedHostStore } from '@/store/seletedHostStore';
 import { HiOutlineHome, HiPlus } from 'react-icons/hi';
-import { Host, Network, ThemeColor } from '@/types/type';
 
 const AddHostButton = () => {
-  const randomId = uuidv4();
-
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [availableNetworks, setAvailableNetworks] = useState<Network[]>([]);
-
-  const { enqueueSnackbar } = useSnackbar();
-  const addHost = useHostStore((state) => state.addHost);
-  const addConnectedBridgeId = selectedHostStore(
-    (state) => state.addConnectedBridgeId
-  );
-
-  // 네트워크 목록을 API에서 가져오는 함수
-  const fetchNetworks = async () => {
-    try {
-      const response = await fetch('/api/network/list');
-      const data = await response.json();
-      console.log('네트워크 리스트 데이터', data);
-      setAvailableNetworks(data || []);
-    } catch (error) {
-      console.error('네트워크 목록을 불러오는데 실패했습니다.', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchNetworks();
-  }, []);
-
-  const handleAddHost = (
-    id: string,
-    hostNm: string,
-    hostIp: string | undefined,
-    isRemote: boolean,
-    themeColor: ThemeColor,
-    networkName: string,
-    networkIp: string
-  ): void => {
-    const newHost: Host = {
-      id,
-      hostNm,
-      hostIp: isRemote ? hostIp : undefined, // isRemote가 true일 때만 hostIp 설정
-      status: true,
-      isRemote,
-      themeColor,
-      networkName,
-      networkIp,
-    };
-
-    const selectedNetwork = availableNetworks.find(
-      (network) => network.Name.toLowerCase() === networkName.toLowerCase()
-    );
-
-    if (!selectedNetwork) {
-      showSnackbar(
-        enqueueSnackbar,
-        '선택된 네트워크를 찾을 수 없습니다.',
-        'error',
-        '#d32f2f'
-      );
-      return;
-    }
-
-    addHost(newHost);
-
-    addConnectedBridgeId(id, {
-      name: selectedNetwork.Name,
-      gateway: selectedNetwork.IPAM?.Config?.[0]?.Gateway || '',
-      driver: selectedNetwork.Driver || '',
-      subnet: selectedNetwork.IPAM?.Config?.[0]?.Subnet || '',
-      scope: selectedNetwork.Scope || '',
-      id: randomId,
-    });
-
-    showSnackbar(
-      enqueueSnackbar,
-      '호스트가 성공적으로 추가되었습니다!',
-      'success',
-      '#254b7a'
-    );
-    setIsModalOpen(false);
-  };
 
   return (
     <>
@@ -107,13 +22,7 @@ const AddHostButton = () => {
         </button>
       </div>
       <div className="min-h-screen flex items-center justify-center">
-        {isModalOpen && (
-          <HostModal
-            onClose={() => setIsModalOpen(false)}
-            onSave={handleAddHost}
-            availableNetworks={availableNetworks}
-          />
-        )}
+        {isModalOpen && <HostModal onClose={() => setIsModalOpen(false)} />}
       </div>
     </>
   );
