cb873988d5db601de5c1a3d8346209361db2ee0b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-30 17:10:57 +0900

    Fix: change base url

--- src/app/api/blueprint/list/route.ts
+++ src/app/api/blueprint/list/route.ts
@@ -1,22 +1,22 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-import { API_URL } from '@/app/api/urlPath';
+import { BASE_URL } from '@/app/api/urlPath';
 
 export async function GET() {
   try {
-    const response = await axios.get(`${API_URL}/blueprints`);
+    const response = await axios.get(`${BASE_URL}/blueprints`);
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
     console.log(error);
     if (error instanceof Error && (error as any).response) {
       return NextResponse.json(
         { error: (error as any).response.data.message },
-        { status: 500 },
+        { status: 500 }
       );
     }
     return NextResponse.json(
       { error: 'Failed to create container' },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
